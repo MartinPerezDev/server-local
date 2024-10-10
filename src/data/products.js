11e46fe605e87568e1d3eb2c84749cece2b80951
unknown <martinperez9713@gmail.com>
2024-10-10 12:07:06 -0300

    first commit

---
+++ src/data/products.js
@@ -0,0 +1,22 @@
+[
+  {
+    "id": 1,
+    "nombre": "Remera Fitz Roy",
+    "descripcion": "",
+    "categoria": "remeras",
+    "stock": 3,
+    "precio": 15000,
+    "imagen": "/img/remera-m.jpg",
+    "timestamp": "10/10/2024 11:47"
+  },
+  {
+    "id": 2,
+    "nombre": "Pantalon Dorian",
+    "descripcion": "",
+    "precio": 720,
+    "stock": 5,
+    "imagen": "/img/pantalon-h.jpg",
+    "categoria": "pantalones",
+    "timestamp": "10/10/2024 11:38"
+  }
+]
